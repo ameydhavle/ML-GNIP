764c08b522a6de8347d5f817440d60c60fb16129
Amey Dhavle <amey.dhavle@marklogic.com>
2013-06-26 14:53:15 -0400

    Rename source

--- ML-GNIP-src/ML-GNIP/Cron Job- Twitter/twitter/twitter-start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-nohup java -jar ML-GNIP-twitter.jar 
--- ML-GNIP-src/ML-GNIP/Cron Job- Twitter/twitter/twitter-stop.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Grabs and kill a process from the pidlist that has the word myapp
-
-pid=`ps ax | grep ML-GNIP-twitter`
-kill $pid
